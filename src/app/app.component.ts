86e6e4b492e2fa29a23b36c49f1e4724bf3042b6
JHON GABRIEL OSPINA OROZCO <jhonospina2711@gmail.com>
2024-05-05 23:01:11 -0500

    Se migra a la nueva version 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { NgFor } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
